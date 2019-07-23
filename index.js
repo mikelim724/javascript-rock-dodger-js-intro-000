d6a07c3259997e1656a85f9557b1d9c1247e4ab4
imperative-cron-3751 <mikelim724@gmail.com>
2019-07-23 08:25:40 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -91,7 +91,7 @@ function createRock(x) {
      * Otherwise, if the rock hasn't reached the bottom of
      * the GAME, we want to move it again.
      */
-    else if (positionToInteger(rock.style.top) < 380) {
+    else if (positionToInteger(rock.style.top) < 380) { //380 bc rock is 20 height.
       window.requestAnimationFrame(moveRock);
     }
     /**
@@ -121,6 +121,7 @@ function createRock(x) {
  */
 function endGame() {
   clearInterval(gameInterval);
+  ROCKS = [];
 }
 
 function moveDodger(e) {

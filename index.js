c105209957061659a5ca1e5a8805e02b936799e5
imperative-cron-3751 <mikelim724@gmail.com>
2019-07-23 07:30:36 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -34,9 +34,9 @@ function checkCollision(rock) {
     const rockLeftEdge = positionToInteger(rock.style.left)
 
     // FIXME: The rock is 20 pixel's wide -- how do we get the right edge?
-    const rockRightEdge = rockLeftEdge +20;
+    const rockRightEdge = rockLeftEdge + 20;
 
-    if (false /**
+    if (      /**
                * Think about it -- what's happening here?
                * There's been a collision if one of three things is true:
                * 1. The rock's left edge is < the DODGER's left edge,
@@ -45,7 +45,11 @@ function checkCollision(rock) {
                *    and the rock's right edge is < the DODGER's right edge;
                * 3. The rock's left edge is < the DODGER's right edge,
                *    and the rock's right edge is > the DODGER's right edge
-               */) {
+               */
+          (rockLeftEdge <= dodgerLeftEdge && rockRightEdge >= dodgerLeftEdge) 
+          || (rockLeftEdge >= dodgerLeftEdge && rockRightEdge <= dodgerRightEdge)
+          || (rockLeftEdge <= dodgerRightEdge && rockRightEdge >= dodgerRightEdge)
+             ) {
       return true
     }
   }

0b2d63e972431b932db7139fcfe7601721411082
imperative-cron-3751 <mikelim724@gmail.com>
2019-07-23 08:54:39 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -123,7 +123,7 @@ function endGame() {
   clearInterval(gameInterval);
   ROCKS = [];
   document.removeEventListener('keydown', moveDodger);
-  return alert("YOU LOSE!");
+  alert("YOU LOSE!");
 }
 
 function moveDodger(e) {

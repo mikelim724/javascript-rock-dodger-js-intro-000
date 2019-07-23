a2e968b4bdd713ed00de3dc8408db9a1ea6de864
imperative-cron-3751 <mikelim724@gmail.com>
2019-07-23 09:24:36 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -139,7 +139,7 @@ function moveDodger(e) {
    */
    e.preventDefault();
    e.stopPropagation();
-   if(e.which ===LEFT_ARROW){
+   if(e.which === LEFT_ARROW){
      window.requestAnimationFrame(moveDodgerLeft);
    }
    else if (e.which ===RIGHT_ARROW) {
